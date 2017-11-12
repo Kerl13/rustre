a7f3490ef143a748b5222f1d6484e5bb81add9a1
Lucas Baudin <lucas.baudin@ens.fr>
2017-11-12 19:52:00 +0100

    Fix normalized ast so as there is no pair types where they should not occur.

--- src/ast_normalized.ml
+++ src/ast_normalized.ml
@@ -2,34 +2,46 @@ open Ast_typed
 open Ast_clocked
 
 (**
+ * Types
+ * tagged with a phantom type
+*)
+type _ sty =
+  | StyBool : bool sty
+  | StyNum  : 'a num_ty -> 'a num_ty sty
+
+(**
  * Expressions
  * tagged with a phantom type corresponding to their lustre type
  * named `a` in the article
 *)
 type 'a nexpr = {
   nexpr_desc  : 'a nexpr_desc ;
-  nexpr_type  : 'a ty ;
+  nexpr_type  : 'a sty;
   nexpr_clock : ct ;
   nexpr_loc   : location
 }
 
 (** named `e` in the article *)
 and 'a nexpr_desc =
-  | CConst : 'a const -> 'a nexpr_desc
-  | CIdent : 'a var_ident -> 'a nexpr_desc
-  | CBOp    : ('a, 'b) binop * 'a nexpr * 'a nexpr -> 'b nexpr_desc
-  | CUOp    : ('a, 'b) unop * 'a nexpr -> 'b nexpr_desc
-  | CWhen  : 'a nexpr * ident * 'b var_ident -> 'a nexpr_desc
-  | CPair  : 'a nexpr * 'b nexpr -> ('a * 'b) nexpr_desc
+  | NConst : 'a const -> 'a nexpr_desc
+  | NIdent : 'a var_ident -> 'a nexpr_desc
+  | NBOp    : ('a, 'b) binop * 'a nexpr * 'a nexpr -> 'b nexpr_desc
+  | NUOp    : ('a, 'b) unop * 'a nexpr -> 'b nexpr_desc
+  | NWhen  : 'a nexpr * ident * 'b var_ident -> 'a nexpr_desc
+  (** not in the article, paired expressions, i.e. lists *)
+and 'a nexpr_paired =
+  | NExpr : 'a nexpr -> 'a nexpr_paired
+  | Nnil  : unit nexpr_paired
+  | NPair : 'a nexpr * 'b nexpr_paired -> ('a * 'b) nexpr_paired
 
 (** named `ce` in the article *)
 type _ nexpr_merge_desc =
-| CMerge : ident * (ident * 'a nexpr_merge) list -> 'a nexpr_merge_desc
+  | NMerge : ident * (ident * 'a nexpr_merge) list -> 'a nexpr_merge_desc
 
 (** named `ca` in the article *)
 and 'a nexpr_merge = {
   nexpr_merge_desc: 'a nexpr_merge_desc;
-  nexpr_merge_type: 'a ty;
+  nexpr_merge_type: 'a sty;
   nexpr_merge_clock: ct;
   nexpr_merge_loc: location;
 }
@@ -38,7 +50,7 @@ and 'a nexpr_merge = {
 (** Programs *)
 type nfile = nnode list
 
-and nnode = Node: ('a, 'b) nnode_desc -> nnode
+and nnode = NNode: ('a, 'b) nnode_desc -> nnode
 
 and ('a, 'b) nnode_desc = {
   n_name   : ('a, 'b) tagged_ident;
@@ -50,6 +62,6 @@ and ('a, 'b) nnode_desc = {
 }
 
 and nequation =
-| EquSimple: 'a pattern * 'a nexpr_merge -> nequation
-| EquFby: 'a pattern * 'a const * 'a nexpr -> nequation
-| EquApp: 'b pattern * ('a, 'b) tagged_ident * 'a nexpr * 'c nexpr -> nequation
+  | EquSimple: 'a pattern * 'a nexpr_merge -> nequation
+  | EquFby: 'a pattern * 'a const * 'a nexpr -> nequation
+  | EquApp: 'b pattern * ('a, 'b) tagged_ident * 'a nexpr_paired * 'c nexpr_paired -> nequation
