e500ab11829514f92c80d1795cc97b1391c3587c
Martin Pépin <kerl@wkerl.me>
2017-12-15 14:51:34 +0100

    Catch lexing and parsing exceptions with -ext

--- src/rustre.ml
+++ src/rustre.ml
@@ -173,7 +173,8 @@ let () =
 
     exit 0
   with
-  | Lexer.Error s ->
+  | Lexer.Error s
+  | Lexer_ext.Error s ->
     report_loc (lexeme_start_p lb, lexeme_end_p lb);
     Format.eprintf "lexical error: %s\n@." s;
     exit 1
@@ -181,7 +182,8 @@ let () =
     report_loc (lexeme_start_p lb, lexeme_end_p lb);
     Format.eprintf "syntax error: %s\n@." s;
     exit 1
-  | Parser.Error ->
+  | Parser.Error
+  | Parser_ext.Error ->
     report_loc (lexeme_start_p lb, lexeme_end_p lb);
     Format.eprintf "syntax error\n@.";
     exit 1
