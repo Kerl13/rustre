1fc56bf51a9a38033ec1cff611bfe8060ef18912
Martin Pépin <kerl@wkerl.me>
2017-12-15 14:21:31 +0100

    Include the extension to the test suite

--- tests/run.sh
+++ tests/run.sh
@@ -7,16 +7,18 @@ fails=0
 total=0
 
 function compile {
+  local ifile=$1
+  local opts=$2
   set -f
-  fst=$(head -n 1 $1)
+  fst=$(head -n 1 $ifile)
   [[ $fst =~ ^"/* main = "([a-zA-Z0-9_]+)" */"$ ]]
   set +f
   MAIN=${BASH_REMATCH[1]}
   if [ "_$MAIN" != "_" ]
   then
-    $COMPILER $1 $MAIN > /dev/null
+    $COMPILER $opts $ifile $MAIN > /dev/null
   else
-    $COMPILER $1 $DEFAULT_MAIN > /dev/null
+    $COMPILER $opts $ifile $DEFAULT_MAIN > /dev/null
   fi
 }
 
@@ -37,6 +39,37 @@ do
     printf "."
   else
     fails=$((fails+1))
+    echo
+  fi
+  total=$((total+1))
+done
+
+echo
+echo "Running positive tests with the extension"
+for file in auto/*.lus
+do
+  compile $file "-ext"
+  if [ $? -eq 0 ]
+  then
+    printf "."
+  else
+    fails=$((fails+1))
+    echo
+  fi
+  total=$((total+1))
+done
+
+echo
+echo "Checking compatibility between standard syntax and extended syntax"
+for file in *.lus
+do
+  compile $file "-ext"
+  if [ $? -eq 0 ]
+  then
+    printf "."
+  else
+    fails=$((fails+1))
+    echo
   fi
   total=$((total+1))
 done
